fa291fc1bbc7b6b15a4b0939a1b975c5f575f4c8
PANENDRAOMMINA <phaneendraommina@gmail.com>
2022-06-05 18:23:29 +0530

    Commit-1
    1.Added the assets
    2.Added some code to Flappy bird script
    3.created pipes prefab without functionality

---
+++ Flappy_Bird/Assets/Scripts/Flappy_bird/FlappyBird.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FlappyBird : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private Vector2 Force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+            rb.AddForce(Force);
+       }
+    }
+}
